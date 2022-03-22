1de0db1445adb7e548e68eddc21de7488f51ca17
Burak Akyüz <burak.akyuz@bilgeadamboost.com>
2022-03-22 12:45:22 +0300

    bootstrap entegrated

--- my-notes-ui/src/App.js
+++ my-notes-ui/src/App.js
@@ -1,27 +1,33 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import { Container } from 'react-bootstrap';
 
 const API_URL = "https://localhost:5001/";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
-  useEffect(()=>{//Başlangıçta ve her update olduğunda, her renderda çalışır.
+  useEffect(() => {//Başlangıçta ve her update olduğunda, her renderda çalışır.
 
     axios.get(API_URL + "api/notes")
       .then(function (response) {
         setNotes(response.data)
       })
 
-  },[]) 
+  }, [])
 
 
   return (
-    <div className="App">
-      <ul>
-        {notes.map((x, i) => <li key={i}>{x.title}</li>)}
-      </ul>
+    <div className='App'>
+      <Container>
+        <h1 className='mt-3'>My Notes</h1>
+        <ul>
+          {notes.map((x, i) => <li key={i}>{x.title}</li>)}
+          <Button variant='secondary'>CLICK ME</Button>
+        </ul>
+      </Container>
     </div>
   );
 }
